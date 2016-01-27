c52959c8d5ea841ae4bc38eda84edbb6fb36d483
dragonboy7878 <mateodragonboy7878@gmail.com>
2016-01-27 13:04:05 -0600

    Fin.

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section03ifs/HiLow.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section03ifs/HiLow.java
@@ -7,29 +7,41 @@ public class HiLow
 {
   public static void main(String[] args)
   {
+    //         If the guess is correct --#4
+    int correct = NumberUtils.getRandomInt(1, 100);
     //    Choose a random number between 1 and 100 --#4.1 (fake!) & --#13
+    MessageBox.showMessage("Choose a random number between 1 and 100.");
     //    Do the following 8 times --#9.1
     for (int i = 0; i < 8; i++)
     {
       //         Ask the user for a guess --#1
-      MessageBox.askForNumericalInput("Make a guess.");
-      //         If the guess is correct --#4
-      int guess = NumberUtils.getRandomInt(1, 100);
-      if (guess == i)
+      int guess = MessageBox.askForNumericalInput("Make a guess.");
+      if (guess == correct)
       {
         //               Play a bell --#2
         //               Tell the user that they won the game  --#3
         MessageBox.showMessage("You won the game!");
         //               and exit --#10
+        System.exit(correct);
       }
       //         Otherwise, if the guess is too high --#6
-      //               Tell the end user that it is too high --#5
-      MessageBox.showMessage("Your guess is too high.");
+      else if (guess > correct)
+      {
+        //               Tell the end user that it is too high --#5
+        MessageBox.showMessage("Your guess is too high.");
+      }
       //         Otherwise, if the guess is too low --#8
-      //               Tell the end user that it is too low --#7
-      MessageBox.showMessage("Your guess is too low.");
+      else if (guess < correct)
+      {
+        //               Tell the end user that it is too low --#7
+        MessageBox.showMessage("Your guess is too low.");
+      }
       //    Repeat --#9.2
+      //    After 8 incorrect guesses tell the user they've lost --#11
+      if (i == 8)
+      {
+        MessageBox.showMessage("You have lost!");
+      }
     }
-    //    After 8 incorrect guesses tell the user they've lost --#11
   }
 }
