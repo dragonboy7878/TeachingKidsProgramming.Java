e63995e1ff56cd09a64633ab9005e5b134a40ca2
dragonboy7878 <mateodragonboy7878@gmail.com>
2016-02-04 13:00:44 -0600

    More edits

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section04mastery/PentagonCrazyQuiz.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section04mastery/PentagonCrazyQuiz.java
@@ -1,6 +1,9 @@
 package org.teachingkidsprogramming.section04mastery;
 
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.ColorWheel;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors.Greens;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors.Reds;
 import org.teachingkidsprogramming.recipes.quizzes.graders.PentagonCrazyQuizGrader;
 import org.teachingkidsprogramming.recipes.quizzes.graders.PentagonQuizAdapter;
 
@@ -10,6 +13,10 @@ public class PentagonCrazyQuiz extends PentagonQuizAdapter
   {
     // Question 1 
     //   createAStitch (recipe below) 
+    createAStitch();
+  }
+  private void createAStitch()
+  {
     //   ------------- Recipe for createAStitch
     //   Move the tortoise 6 pixels
     Tortoise.move(6);
@@ -19,18 +26,24 @@ public class PentagonCrazyQuiz extends PentagonQuizAdapter
   public void question2()
   {
     //        Do the following 76 times
-    //        Call sewAStitch
-    //        Repeat
+    for (int i = 0; i < 76; i++)
+    {
+      //        Call sewAStitch
+      sewAStitch();
+      //        Repeat
+    }
   }
   @Override
   public void question3()
   {
     //        Add lime to the color wheel
+    ColorWheel.addColor(Greens.Lime);
   }
   @Override
   public void question4()
   {
     //        Add red to the color wheel
+    ColorWheel.addColor(Reds.Red);
   }
   public static void main(String[] args)
   {
